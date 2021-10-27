2c2dd9a174b547948c0990c051b0b1c21f0434a5
JS <justinsbits@gmail.com>
2021-10-27 16:06:41 -0700

    DbContextOptions - explicit type <AppDbContext>

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -5,7 +5,7 @@ namespace CommanderDA
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
